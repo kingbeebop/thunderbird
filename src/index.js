390448058689370105bfc0eda3b0a6a48a6c0e50
Brendan Ward <kingbeebop@gmail.com>
2022-06-01 10:30:51 -0400

    game logic working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+
+//set global variables
 const person = {
     name: 'sara',
     likes: ['one','two','three'],
@@ -12,6 +14,7 @@ const gameText = document.getElementById('game-text')
 const gameButtons = document.getElementById('game-buttons')
 let currentTime = 0
 
+//fetch data and initiate game
 fetch("http://localhost:3000/game")
 .then(res=>res.json())
 .then(data => {
@@ -19,16 +22,19 @@ fetch("http://localhost:3000/game")
     runGame(data)
 })
 
+//render graphics
 function render(){
     displayTitle.textContent = person.name
     displayImage.src = person.image
 }
 
+//generage new random person
 function setPerson(){
   fetch('https://random-data-api.com/api/name/random_name')
   .then(res=>res.json())
   .then(json=>{
       person.name = json.name
+      //TODO: randomize this
       person.likes = ['coffee', 'neo-soul', 'archery']
       person.mood = Math.random() * 100
       render(person)
@@ -36,15 +42,25 @@ function setPerson(){
   
 }
 
-//TODO do this part:
-
+//game engine:
 function runGame(gameData){
-    
-    gameText.textcontent = gameData.intoText
-    //FIX THIS PART:
+ 
+    if(!gameData[currentTime]){
+        endGame()
+        return
+    }
+
+    //get current game paragraph
+    console.log(gameData[currentTime].text)
+    gameText.textContent = gameData[currentTime].text
+
+    //set up interface:
+    clearButtons()
+
+    //populate new game options
     let turn = gameData[currentTime].options
+
     turn.forEach(option =>{
-        console.log("test")
         let newButton = document.createElement('btn')
         newButton.textContent = option
         newButton.addEventListener('click',()=>{
@@ -54,11 +70,49 @@ function runGame(gameData){
         gameButtons.appendChild(newButton)
     })
 
+    //move to next game time interval
     currentTime ++
 
 }
 
+//endgame
+function endGame(){
+    let mood = person.mood
+    if(mood>80){
+        gameText.textContent = `${person.name} had a really great day!  Thanks for everything!`
+    }
+    else if(mood>60){
+        gameText.textContent = `${person.name} had an OK day!  Thanks for playing.`
+    }
+    else if(mood>40){
+        gameText.textContent = `${person.name}'s day was alright.  Thanks, I guess.`
+    }
+    else if(mood>20){
+        gameText.textContent = `${person.name} had a pretty middling day.  Whatever.`
+    }
+    else{
+        gameText.textContent = `${person.name} had an absolutely awful day.  Thanks for nothing, jerk.`
+
+    }
+
+    //create a reset button
+    clearButtons()
+    let resetButton = document.createElement('btn')
+    resetButton.addEventListener('click',()=>{
+        window.location.reload()
+    })
+    resetButton.textContent = 'TRY AGAIN'
+    gameButtons.appendChild(resetButton)
+}
+
+//checks if an option matches person's interests 
 function likeCheck(option){
+    console.log(option)
+    //if the button option is 'next', do nothing
+    if(option.toLowerCase() == "next"){
+        return
+    }
+    //if button matches any of person's interests, add to mood
     person.likes.forEach(like => {
         if(like == option){
             person.mood += 10
@@ -66,4 +120,11 @@ function likeCheck(option){
         }
     })
     person.mood -= 10
+}
+
+//clears game console
+function clearButtons(){
+    while(gameButtons.firstChild){
+        gameButtons.removeChild(gameButtons.firstChild)
+    }
 }
